63035935947ba036d97a03091cbafe3750806c1a
Manish Bhatt <manishbhatt594@gmail.com>
2025-10-03 17:42:57 +0530

    Whitespace formatted

--- JavaBasics/src/com/shrishti/arrays/TwoDimArr.java
+++ JavaBasics/src/com/shrishti/arrays/TwoDimArr.java
@@ -4,7 +4,7 @@ public class TwoDimArr {
 
 	public static void main(String[] args) {
 		int[][] nums = new int[3][4];
-		
+
 		nums[0][0] = 0;
 		nums[0][1] = 1;
 		nums[0][2] = 2;
@@ -14,30 +14,29 @@ public class TwoDimArr {
 		nums[1][1] = 5;
 		nums[1][2] = 6;
 		nums[1][3] = 7;
-		
+
 		nums[2][0] = 8;
 		nums[2][1] = 9;
 		nums[2][2] = 10;
 		nums[2][3] = 11;
-		
+
 		for (int i = 0; i < nums.length; i++) {
 			for (int j = 0; j < nums[i].length; j++) {
 				System.out.print(nums[i][j] + " ");
 			}
 			System.out.println();
 		}
-		
-		
+
 		// Compute sum of all elements in the 2D array
 		System.out.println();
 		int sum = 0;
-		
-		for (int[] row: nums) {
-			for (int val: row) {
+
+		for (int[] row : nums) {
+			for (int val : row) {
 				sum += val;
 			}
 		}
-		
+
 		System.out.println("Sum = " + sum);
 	}
 }
