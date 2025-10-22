e43e8d1cfa76ff88a2609c92c242de6e0a51f376
Manish Bhatt <manishbhatt594@gmail.com>
2025-10-22 16:46:57 +0530

    Add example for ClassCastException

--- JavaDevRecordedLectures01/src/com/lecturecat/classtypecast/Billing.java
+++ JavaDevRecordedLectures01/src/com/lecturecat/classtypecast/Billing.java
@@ -15,6 +15,22 @@ public class Billing {
 		UpiPayment upi = (UpiPayment) payment;
 		upi.validateUpiId(1234);
 		System.out.println();
+		
+		// Example of ClassCastException
+		// 'payment' reference is currently pointing to a UpiPayment object.
+		// If we were mistakenly thinking that it is pointing to a CreditCardPayment object,
+		// and accordingly to call own methods of the CreditCardPayment class, we try to
+		// cast the 'payment' reference to CreditCardPayment, on runtime we will get a
+		// ClassCastException
+		// Note that compiler is not able to catch this issue
+		// CreditCardPayment creditRef = (CreditCardPayment) payment;
+		/**
+		 * Exception in thread "main" java.lang.ClassCastException:
+		 * class com.lecturecat.classtypecast.UpiPayment cannot be cast to
+		 * class com.lecturecat.classtypecast.CreditCardPayment
+		 * (com.lecturecat.classtypecast.UpiPayment and com.lecturecat.classtypecast.CreditCardPayment are in module JavaDevRecordedLectures01 of loader 'app')
+		 * at JavaDevRecordedLectures01/com.lecturecat.classtypecast.Billing.main(Billing.java:26)
+		 */
 
 		payment = new CreditCardPayment();
 		payment.processPayment(4500.0);
