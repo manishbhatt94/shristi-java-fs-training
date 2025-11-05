45228ee2ede469c7189c9023a8d5283246c3a651
Manish Bhatt <manishbhatt594@gmail.com>
2025-11-05 13:20:47 +0530

    Add demo for try block with multiple catch blocks

---
+++ ExceptionHandlingDemos/src/com/excep/basics/MultipleDemo.java
@@ -0,0 +1,39 @@
+package com.excep.basics;
+
+public class MultipleDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		try {
+			String val = args[0]; // ArrayIndexOutOfBoundsException
+			System.out.println("Got value: " + val);
+			int num = Integer.parseInt(val); // NumberFormatException
+			System.out.println("Number: " + num);
+			int result = 10 / num; // ArithmeticException
+			int[] marks = null;
+			System.out.println(marks[0]); // NullPointerException
+			System.out.println("Result: " + result);
+		} catch (ArithmeticException e) {
+			System.out.println("Don't enter zero");
+			System.out.println("Error occured");
+			System.out.println(e);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Please enter value");
+			System.out.println("Error occured");
+			System.out.println(e);
+		} catch (NumberFormatException e) {
+			System.out.println("Please enter a positive integer");
+			System.out.println("Error occured");
+			System.out.println(e);
+		} catch (RuntimeException e) {
+			System.out.println("RuntimeException");
+			System.out.println("Error occured");
+			System.out.println(e);
+		} catch (Exception e) {
+			System.out.println("Other exception");
+			System.out.println("Error occured");
+			System.out.println(e);
+		}
+		System.out.println("Work done");
+	}
+}
