27366a1a8f7db888fe795bd695f9971611ec55ef
Manish Bhatt <manishbhatt594@gmail.com>
2025-11-05 13:18:40 +0530

    Update ATM Exception example to demo use of throws for checked exception

--- ExceptionHandlingDemos/src/com/excep/throwsdemo/Bank.java
+++ ExceptionHandlingDemos/src/com/excep/throwsdemo/Bank.java
@@ -2,11 +2,13 @@ package com.excep.throwsdemo;
 
 public class Bank {
 
-	void withdraw(int amount) {
+	void withdraw(int amount) throws Exception {
 		System.out.println("In Bank");
 		try {
-			int balance = 5000 / amount;
-			System.out.println("Balance: " + balance);
+			if (amount <= 0) {
+				throw new Exception();
+			}
+			System.out.println("Balance: " + amount);
 		} catch (Exception e) {
 			System.out.println("Technical error");
 			throw e;
