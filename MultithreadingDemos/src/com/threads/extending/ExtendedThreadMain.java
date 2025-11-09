4e857569294c1d0ba0de4bf56437f98eb0cca7d5
Manish Bhatt <manishbhatt594@gmail.com>
2025-11-09 11:57:15 +0530

    Example of multiple instances of Thread subclass

--- MultithreadingDemos/src/com/threads/extending/ExtendedThreadMain.java
+++ MultithreadingDemos/src/com/threads/extending/ExtendedThreadMain.java
@@ -12,10 +12,19 @@ class Child extends Thread {
 public class ExtendedThreadMain {
 
 	public static void main(String[] args) {
-		Child t = new Child();
-		t.setName("Thread-1");
-		t.setPriority(Thread.MAX_PRIORITY - 1);
-		t.start();
-		t.run();
+		Child t1 = new Child();
+		t1.setName("Thread-1");
+		t1.setPriority(Thread.MAX_PRIORITY - 1);
+		t1.start();
+
+		Child t2 = new Child();
+		t2.setName("Thread-2");
+		t2.setPriority(Thread.MAX_PRIORITY - 1);
+		t2.start();
+
+		Child t3 = new Child();
+		t3.setName("Thread-3");
+		t3.setPriority(Thread.MAX_PRIORITY - 1);
+		t3.start();
 	}
 }
